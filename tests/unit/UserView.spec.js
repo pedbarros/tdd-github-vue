88c39ba40bd4da711ad7e69d6168c369484963c8
Pedro Barros <pedro.barros@unimedmaceio.com.br>
2019-05-07 09:05:15 -0300

    First failed UserView.spec.js

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,3 +1,17 @@
+import { shallowMount } from '@vue/test-utils'
+import UserView from '@/views/UserView'
+
 describe('UserView', () => {
   it('works', () => {})
+})
+
+
+describe('UserView', () => {
+  it('renders the component', () => {
+    // arrange
+    const wrapper = shallowMount(UserView)
+    
+    //assert
+    expect(wrapper.html()).toMatchSnapshot()
+  })
 })
