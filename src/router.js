add135bd16a14d7055c8afe1c3cdcdfac88a194d
Pedro Barros <pedro.barros@unimedmaceio.com.br>
2019-05-07 14:02:33 -0300

    Add test service api github

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const UserView = () => import('@/views/UserView')
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      component: UserView
+    }
+  ]
+})
