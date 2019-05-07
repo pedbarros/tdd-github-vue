517dd4bebcffd3fb013b0babf0e737454b42d962
Pedro Barros <pedro.barros@unimedmaceio.com.br>
2019-05-07 09:44:32 -0300

    Refactor test component UserView

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -5,23 +5,31 @@ import VUserProfile from '@/components/VUserProfile'
 
 // testar se o componente está renderizando
 describe('UserView', () => {
+
+  const build = () => {
+    const wrapper = shallowMount(UserView)
+
+    return {
+      wrapper,
+      userSearchForm: () => wrapper.find(VUserSearchForm),
+      userProfile: () => wrapper.find(VUserProfile)
+    }
+  }
+
   it('renders the component', () => {
     // arrange
-    const wrapper = shallowMount(UserView)
-    
+    const { wrapper } = build() 
     //assert
     expect(wrapper.html()).toMatchSnapshot()
   })
 
 
-  it('renders main child components', () => {
-    const wrapper = shallowMount(UserView)
-    const userSearchForm = wrapper.find(VUserSearchForm)
-    const userProfile = wrapper.find(VUserProfile)
+  it('renders main child components', () => { 
+    const {userSearchForm, userProfile} =  build() 
 
-    expect(userSearchForm.exists()).toBe(true)
-    expect(userProfile.exists()).toBe(true)
-    
+    expect(userSearchForm().exists()).toBe(true)
+    expect(userProfile().exists()).toBe(true)
+
 
   })
 })
